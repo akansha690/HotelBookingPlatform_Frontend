d30293002648739640cce3f58056706db163df19
akansha <akansha69@.com>
2026-06-04 09:57:33 +0530

    add changes

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   preview: {
     port: parseInt(process.env.PORT) || 4173,
     host: true,
-    allowedHosts: ['hotelbookingplatformfrontend-production.up.railway.app']
+    allowedHosts: "hotelbookingplatformfrontend-production.up.railway.app"
   }
 })
