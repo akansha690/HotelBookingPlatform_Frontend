6b75840c3fbd748b3c5304ce052d0235592d1423
akansha <akansha69@.com>
2026-06-05 10:02:26 +0530

    add changes in .env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 axiosInstance.interceptors.request.use((config) => {
@@ -14,4 +14,4 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
-export default axiosInstance;
+export default axiosInstance; 
